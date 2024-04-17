c4504eb0e5a43f6721193d71724965a611090714
Santiago Andres Baquero Cortes <baquerosantiago12@gmail.com>
2024-04-17 11:00:57 -0500

    Actualizacion tipo de dato caracter

--- TiposDeDato/src/EjemploCaracteres.java
+++ TiposDeDato/src/EjemploCaracteres.java
@@ -4,10 +4,18 @@ public class EjemploCaracteres {
         // Solo soporta 1 caracter
         char caracter ='S'; //0-9
         System.out.println("caracter = " + caracter);
-        char arroba='\u0040';
+        char arroba='\u0040'; // Valor de arroba
         System.out.println("arroba = " + arroba);
+    
+        char decimal=64; //Se imprime la Arroba igualmente
+        System.out.println("decimal = " + decimal);
 
-        
-        
+        System.out.println("Decimal = Arroba: "+ (arroba == decimal));// Comparamos si es el mismo valor
+
+        char simbolo= '@';
+        System.out.println("simbolo == Decimal " + (simbolo == decimal));
+
+
+
     }
 }
