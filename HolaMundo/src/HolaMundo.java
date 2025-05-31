7d4d14744c6c2446c4f8fce12c6dd8133babba40
Santiago Andres Baquero Cortes <baquerosantiago12@gmail.com>
2025-05-31 12:43:53 -0500

    Anotaciones HolaMundo

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -1,13 +1,10 @@
 public class HolaMundo {
     public static void main(String[] args){
 
-        System.out.println("Hola Mundo");
-        String saludar = "Hola mundo en variable";
-                System.out.println("saludar = "+saludar.toUpperCase());
-        /* ToUppercase lo convierte en mayuscula */
-
-        int numero = 10;
-        System.out.println("numero = " + numero);
+        System.out.println(" "); /* Espacio al inicio del contenido del codigo */
+        System.out.println("Hola Mundo"); /* Imprime hola mundo en consola */
+        String saludar = "Hola mundo (En Variable)"; /* Asiganamos Hola mundo en una valiable */
+        System.out.println("saludar = "+saludar.toUpperCase()); /* ToUppercase convierte el valor de la variable en mayuscula */
 
 
 
